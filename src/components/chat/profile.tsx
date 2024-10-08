5cc3bfab33ca6ad805b980b232ff02f806a47d94
oliver <oliver0709@gmail.com>
2024-10-09 01:24:22 +0200

    D3 messages+footer+header+mchats+message-rectang e iconos

--- src/components/chat/profile.tsx
+++ src/components/chat/profile.tsx
@@ -1,5 +1,17 @@
+import { Button } from "@/components/ui/button";
+
 
 const Profile = () => {
-    return <div className="bg-orange-100">Profile</div>;
+    return (<div className="p-4 text-center border-l">
+              <img src="https://randomuser.me/api/portraits/women/12.jpg"
+              alt="profile inside chat" className="rounded-md mb-4 mx-auto w-24 h-20"/>
+
+              <h2 className="text-xl font-bold text-gray-700 mb-4">Profile</h2> 
+                <p className="font-semibold">Juan Perez</p>
+                <p className="text-gray-500 mb-2">juan@domain.com</p>
+
+                <Button className="w-full">Logout</Button>
+
+            </div>);
   };
   export default Profile;
